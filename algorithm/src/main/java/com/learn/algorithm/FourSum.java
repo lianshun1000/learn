f8be0dc96def30a194a4e66bc0e9c8f39ba457f2
lianshun1000 <1035950489@qq.com>
2021-02-25 00:03:39 +0800

    four sum

--- algorithm/src/main/java/com/learn/algorithm/FourSum.java
+++ algorithm/src/main/java/com/learn/algorithm/FourSum.java
@@ -1,12 +1,10 @@
 package com.learn.algorithm;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class FourSum {
     public static void main(String[] args) {
-        int[] nums = {1, 0, -1, 0, -2, 2};
+        int[] nums = {-3,-2,-1,0,0,1,2,3};
         int target = 0;
         System.out.println(fourSum(nums,target));
     }
@@ -18,11 +16,39 @@ public class FourSum {
      * @return
      */
     public static List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> results = new ArrayList<>();
-
+        Set<List<Integer>> results = new HashSet<>();
+        //List<List<Integer>> resultList = new ArrayList<>();
         Arrays.sort(nums);
         final int size = nums.length;
+        for (int i = 0; i < size-3; i++) {
+           int target2 = target - nums[i];
+            for (int j = i+1; j < size - 2; j++) {
+                int baseVal = nums[j];
+                int secPr = size - 1;
+                int firPr = j + 1;
+                while (secPr > firPr) {
+                    int comp = target2 - baseVal - nums[firPr] - nums[secPr];
+                    if (comp == 0) {
+                       List<Integer> list = new ArrayList<>();
+                       list.add(nums[i] );
+                       list.add(nums[j]);
+                       list.add(nums[firPr]);
+                       list.add(nums[secPr]);
+                       results.add(list);
+                        firPr ++;
+                    }
+                    if(comp < 0){
+                        secPr --;
+                    }
+                    if(comp > 0){
+                        firPr ++;
+                    }
+                }
 
-        return null;
+            }
+
+        }
+
+        return new ArrayList<>(results);
     }
 }
